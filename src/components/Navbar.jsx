e621a54f034b7ea383ebf99f2f43adec1c58f7bf
AtharvaDondal <120319760+AtharvaDondal@users.noreply.github.com>
2024-03-30 09:55:09 +0530

    improve some changes and final commit for iTask.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-<nav className="flex justify-between bg-indigo-900 text-white py-2">
+<nav className="flex justify-between bg-indigo-900 text-white py-3">
       <div className="logo">
         <span className="font-bold text-xl mx-8">iTask</span>
       </div>
